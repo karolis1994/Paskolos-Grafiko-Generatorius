10a7f47aa72f1f7ce9090f1ea62840d0ac63c2e3
karolis1994 <karolispock@gmail.com>
2016-01-20 17:45:16 +0200

    Pilnas-Projektas

---
+++ PapildomaUzduotis2/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace PapildomaUzduotis2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startingDateLabel = new System.Windows.Forms.Label();
+            this.loanLabel = new System.Windows.Forms.Label();
+            this.interestRateLabel = new System.Windows.Forms.Label();
+            this.paymentCountLabel = new System.Windows.Forms.Label();
+            this.loanTextBox = new System.Windows.Forms.TextBox();
+            this.interestRateTextBox = new System.Windows.Forms.TextBox();
+            this.paymentsCountTextBox = new System.Windows.Forms.TextBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.startingDateTextBox = new System.Windows.Forms.TextBox();
+            this.dateFormatLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startingDateLabel
+            // 
+            this.startingDateLabel.AutoSize = true;
+            this.startingDateLabel.Location = new System.Drawing.Point(12, 9);
+            this.startingDateLabel.Name = "startingDateLabel";
+            this.startingDateLabel.Size = new System.Drawing.Size(72, 13);
+            this.startingDateLabel.TabIndex = 0;
+            this.startingDateLabel.Text = "Starting Date:";
+            // 
+            // loanLabel
+            // 
+            this.loanLabel.AutoSize = true;
+            this.loanLabel.Location = new System.Drawing.Point(12, 35);
+            this.loanLabel.Name = "loanLabel";
+            this.loanLabel.Size = new System.Drawing.Size(34, 13);
+            this.loanLabel.TabIndex = 1;
+            this.loanLabel.Text = "Loan:";
+            // 
+            // interestRateLabel
+            // 
+            this.interestRateLabel.AutoSize = true;
+            this.interestRateLabel.Location = new System.Drawing.Point(12, 61);
+            this.interestRateLabel.Name = "interestRateLabel";
+            this.interestRateLabel.Size = new System.Drawing.Size(68, 13);
+            this.interestRateLabel.TabIndex = 2;
+            this.interestRateLabel.Text = "Interest Rate";
+            // 
+            // paymentCountLabel
+            // 
+            this.paymentCountLabel.AutoSize = true;
+            this.paymentCountLabel.Location = new System.Drawing.Point(12, 87);
+            this.paymentCountLabel.Name = "paymentCountLabel";
+            this.paymentCountLabel.Size = new System.Drawing.Size(87, 13);
+            this.paymentCountLabel.TabIndex = 3;
+            this.paymentCountLabel.Text = "Payments Count:";
+            // 
+            // loanTextBox
+            // 
+            this.loanTextBox.Location = new System.Drawing.Point(110, 35);
+            this.loanTextBox.Name = "loanTextBox";
+            this.loanTextBox.Size = new System.Drawing.Size(100, 20);
+            this.loanTextBox.TabIndex = 5;
+            this.loanTextBox.TextChanged += new System.EventHandler(this.loanTextBox_TextChanged);
+            // 
+            // interestRateTextBox
+            // 
+            this.interestRateTextBox.Location = new System.Drawing.Point(110, 61);
+            this.interestRateTextBox.Name = "interestRateTextBox";
+            this.interestRateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.interestRateTextBox.TabIndex = 6;
+            this.interestRateTextBox.TextChanged += new System.EventHandler(this.interestRateTextBox_TextChanged);
+            // 
+            // paymentsCountTextBox
+            // 
+            this.paymentsCountTextBox.Location = new System.Drawing.Point(110, 87);
+            this.paymentsCountTextBox.Name = "paymentsCountTextBox";
+            this.paymentsCountTextBox.Size = new System.Drawing.Size(100, 20);
+            this.paymentsCountTextBox.TabIndex = 7;
+            this.paymentsCountTextBox.TextChanged += new System.EventHandler(this.paymentsCountTextBox_TextChanged);
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(110, 113);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 23);
+            this.generateButton.TabIndex = 8;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // startingDateTextBox
+            // 
+            this.startingDateTextBox.Location = new System.Drawing.Point(110, 9);
+            this.startingDateTextBox.Name = "startingDateTextBox";
+            this.startingDateTextBox.Size = new System.Drawing.Size(100, 20);
+            this.startingDateTextBox.TabIndex = 4;
+            // 
+            // dateFormatLabel
+            // 
+            this.dateFormatLabel.AutoSize = true;
+            this.dateFormatLabel.Location = new System.Drawing.Point(216, 9);
+            this.dateFormatLabel.Name = "dateFormatLabel";
+            this.dateFormatLabel.Size = new System.Drawing.Size(117, 13);
+            this.dateFormatLabel.TabIndex = 9;
+            this.dateFormatLabel.Text = "Format: YYYY/MM/DD";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(191, 118);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(115, 13);
+            this.resultLabel.TabIndex = 10;
+            this.resultLabel.Text = "Type in the parameters";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 149);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.dateFormatLabel);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.paymentsCountTextBox);
+            this.Controls.Add(this.interestRateTextBox);
+            this.Controls.Add(this.loanTextBox);
+            this.Controls.Add(this.startingDateTextBox);
+            this.Controls.Add(this.paymentCountLabel);
+            this.Controls.Add(this.interestRateLabel);
+            this.Controls.Add(this.loanLabel);
+            this.Controls.Add(this.startingDateLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label startingDateLabel;
+        private System.Windows.Forms.Label loanLabel;
+        private System.Windows.Forms.Label interestRateLabel;
+        private System.Windows.Forms.Label paymentCountLabel;
+        private System.Windows.Forms.TextBox loanTextBox;
+        private System.Windows.Forms.TextBox interestRateTextBox;
+        private System.Windows.Forms.TextBox paymentsCountTextBox;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.TextBox startingDateTextBox;
+        private System.Windows.Forms.Label dateFormatLabel;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ PapildomaUzduotis2/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PapildomaUzduotis2
+{
+    public partial class Form1 : Form
+    {
+
+        private DateTime _date;
+        private float _loan;
+        private float _interestRate;
+        private int _paymentCount;
+        private StringBuilder csv = new StringBuilder();
+        private float _monthlyRate;
+        private float _monthlyPayment;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loanTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!checkIfInt32(loanTextBox.Text))
+                loanTextBox.Text = string.Empty;
+        }
+
+        private void interestRateTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(interestRateTextBox.Text, "^[0-9]+$"))
+                interestRateTextBox.Text = string.Empty;
+            else if (Int32.Parse(interestRateTextBox.Text) > 100)
+                interestRateTextBox.Text = string.Empty;
+        }
+
+        private void paymentsCountTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if(!checkIfInt32(paymentsCountTextBox.Text))
+                paymentsCountTextBox.Text = string.Empty;
+        }
+
+        private bool checkIfInt32(string text)
+        {
+            int number;
+            if (!System.Text.RegularExpressions.Regex.IsMatch(text, "^[0-9]+$"))
+                return false;
+            if (!Int32.TryParse(text, out number))
+                return false;
+            return true;
+        }
+
+        private bool CheckIfDate(string text)
+        {
+            if (!DateTime.TryParseExact(text, "yyyy/mm/dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out _date))
+                return false;
+            return true;
+        }
+
+        private bool CheckIfParametersNotEmpty()
+        {
+            if (startingDateTextBox.Text == string.Empty)
+                return false;
+            if (loanTextBox.Text == string.Empty)
+                return false;
+            if (interestRateTextBox.Text == string.Empty)
+                return false;
+            if (paymentsCountTextBox.Text == string.Empty)
+                return false;
+            return true;
+        }
+
+        private void AssignAllParameters()
+        {
+            string s1 = "Payment #";
+            string s2 = "Payment date";
+            string s3 = "Remaining amount";
+            string s4 = "Principal payment";
+            string s5 = "Interest payment";
+            string s6 = "Total payment";
+            string s7 = "Interest rate";
+            string newLine = string.Format("{0},{1},{2},{3},{4},{5},{6}", s1, s2, s3, s4, s5, s6, s7);
+            csv.AppendLine(newLine);
+            _loan = Int32.Parse(loanTextBox.Text);
+            _interestRate = Int32.Parse(interestRateTextBox.Text);
+            _paymentCount = Int32.Parse(paymentsCountTextBox.Text);
+            _interestRate = Int32.Parse(interestRateTextBox.Text);
+            _monthlyRate = _interestRate / 100 / 12;
+            _monthlyPayment = (float)((_monthlyRate / (1 - (Math.Pow((1 + _monthlyRate), -(_paymentCount))))) * _loan);
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            if (!CheckIfParametersNotEmpty())
+            {
+                resultLabel.Text = "Type in the parameters";
+                return;
+            }
+            if (!CheckIfDate(startingDateTextBox.Text))
+            {
+                startingDateTextBox.Text = string.Empty;
+                resultLabel.Text = "Incorrect date format";
+                return;
+            }
+            AssignAllParameters();
+            int paymentNum = 0;
+            for (var i = 0; i < _paymentCount; i++)
+            {
+                paymentNum++;
+                float interestForMonth = _loan*_monthlyRate;
+                float principalForMonth = _monthlyPayment - interestForMonth;
+                string newLine = string.Format("{0},{1},{2},{3},{4},{5},{6}", paymentNum,
+                    _date.ToShortDateString(), _loan.ToString("0.00"), principalForMonth.ToString("0.00"),
+                    interestForMonth.ToString("0.00"), (principalForMonth + interestForMonth).ToString("0.00"),
+                    _interestRate);
+                _loan -= principalForMonth;
+                csv.AppendLine(newLine);
+                _date = _date.AddMonths(1);
+            }
+            File.WriteAllText(Directory.GetCurrentDirectory() + "\\Uzduotis.csv", csv.ToString());
+            resultLabel.Text = "The file has been generated";
+        }
+
+    }
+}
